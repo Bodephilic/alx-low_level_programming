9a898ebeb567c07b085cecd1ad0af7a7f2fba48c
Bodephilic <johnsonbabarinde1@gmail.com>
2023-03-08 08:26:11 -0800

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function that print strings
+ * follow by new line
+ * @s: variable input address
+ * Return:void
+ */
+void _puts_recursion(char *s)
+{
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('s');
+	_putchar(' ');
+	_putchar('w');
+	_putchar('i');
+	_putchar('t');
+	_putchar('h');
+	_putchar(' ');
+	_putchar('r');
+	_putchar('e');
+	_putchar('c');
+	_putchar('u');
+	_putchar('r');
+	_putchar('s');
+	_putchar('i');
+	_putchar('o');
+	_putchar('n');
+}
